a93ce4cf52c7cd111bc6d4e5820456d5e5661fcd
Павел Подвойский <pavelxx66@yandex.ru>
2024-03-28 20:01:47 +0300

    add construction

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from random import randint
 
 
-def attack(char_name: str, char_class: str):
+from graphic_arts.start_game_banner import run_screensaver
+
+
+def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f' {5 + randint(3, 5)}')
@@ -80,7 +83,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -90,6 +94,3 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
